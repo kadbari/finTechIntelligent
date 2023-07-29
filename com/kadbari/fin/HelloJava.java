f311caab01ccb14d4dcaa925d8fd3b1a52005131
ArunRagavendar <armyar@gmail.com>
2023-07-29 21:03:16 +0530

    Added Hello "World" Java

--- com/kadbari/fin/HelloJava.java
+++ com/kadbari/fin/HelloJava.java
@@ -2,6 +2,7 @@ package com.kadbari.fin;
 
 public class HelloJava {
     public static void main(String args[]){
-        System.out.println("Hello Java");
+        System.out.println("Hello World Java");
     }
+
 }
